4689099d347ee6cc924b7f609536acfddbb9a22f
Leticia <lalvessi@calpoly.edu>
2022-05-26 11:17:02 -0400

    staff login

--- frontend/banking/src/App.js
+++ frontend/banking/src/App.js
@@ -17,19 +17,20 @@ import StaffLogin from "./components/Staff/StaffLogin";
 //localStorage.removeItem(key);
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isStaffLoged, setStaffLogged] = useState(false);
 
     return (
         <React.StrictMode>
             <BrowserRouter>
                 {/*<Navigation/>*/}
                 <Routes>
-                    <Route path="/" element={ isLoggedIn ? <DashCustomer setIsLoggedIn={setIsLoggedIn}/> : <Landing setIsLoggedIn={setIsLoggedIn}/> }/>
+                    <Route path="/" element={isLoggedIn ? <DashCustomer setIsLoggedIn={setIsLoggedIn}/> : <Landing setIsLoggedIn={setIsLoggedIn}/>}/>
                     <Route path="/questionAuth" element={<ForgotPassword/>}/>
                     <Route path="/updatePassword" element={<UpdatePassword/>}/>
                     <Route path="/infoMismatch" element={<SecQuesMismatch/>}/>
                     <Route path="/profile" element={<Profile/>}/>
                     <Route path="/createAccount" element={<CreateAccount/>}/>
-
+                    <Route path="/staffCorner" element={<StaffLogin/>}/>
                     <Route path="/DashCustomer" element={<DashCustomer/>}/>
                     <Route path="/staffLogin" element={<StaffLogin/>}/>
 
