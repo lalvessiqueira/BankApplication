8523775afa59dee30210c9c22b70272fb6d8d709
HenrryWith2Rs <50603179+HenrryWith2Rs@users.noreply.github.com>
2022-05-25 21:33:22 -0400

    Several cosmetic changes, added warning on customer dash

--- frontend/banking/src/components/CustomerDash/CustomerFunctions/ProfileWarning.js
+++ frontend/banking/src/components/CustomerDash/CustomerFunctions/ProfileWarning.js
@@ -1,15 +1,11 @@
 import React, {Component} from "react";
-import CreateAccount from "./CreateAccount";
-import AddBeneficiary from "./AddBeneficiary";
-import RemoveBeneficiary from "./RemoveBeneficiary";
-import Profile from "./Profile";
-import TransferMoney from "./TransferMoney";
-import ViewStatement from "./ViewStatement";
+
 
 export default function ProfileWarning() {
     return (
-        <div>
-            <h1>Please fill out profile information!</h1>
+        <div className="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Suggestion!</strong> Update the info in your PROFILE to access more features.
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     )
 }
